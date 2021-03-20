c22bd0dd61391d074ff4ff4715c16eac7074aa06
Kanui <marley.kanui@gmail.com>
2021-03-20 09:43:43 -0500

    django - global commit 05 - displaying DB content in using Django

--- vote/votes/views.py
+++ vote/votes/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Question, Choice
 
-# Create your views here.
+# Get Questions and display them
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'votes/index.html', context)
+
+
+
