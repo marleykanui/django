d52ae15b8a6c0c67d6029ec57532c3de8b11a896
Kanui <marley.kanui@gmail.com>
2021-03-20 10:11:33 -0500

    django - global commit 07 - set up vote view controller

--- vote/votes/views.py
+++ vote/votes/views.py
@@ -1,4 +1,9 @@
-from django.shortcuts import render
+
+from django.template import loader
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+
 from .models import Question, Choice
 
 # Get Questions and display them
@@ -13,9 +18,24 @@ def detail(request, question_id):
         question = Question.objects.get(pk=question_id)
     except Question.DoesNotExist:
         raise Http404("Question does not exist")
-    return render(request, 'votes/results.html', {'question': question})
+    return render(request, 'votes/detail.html', {'question': question})
 
 # Get question and display results
 def results(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'votes/results.html', {'question': question})
+
+# Vote for a question choice
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'votes/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('votes:results', args=(question.id,)))
